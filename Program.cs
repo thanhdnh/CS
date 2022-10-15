8c3b2c4a36d9a7fc4e7384c0d1a5805aa529090d
thanhdnh <myhoangthanh@yahoo.com>
2022-10-15 16:08:36 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -1,32 +1,40 @@
 ﻿public class Program
 {
-  static void RandomizeArray(int[] arr){
-    Random r = new Random();
-    for(int i=0; i<arr.Length; i++)
-      arr[i] = r.Next(1, 9999);
+  static ulong TinhTong1(int n){//dùng if ktra chẵn
+    ulong sum = 0;
+    for(int i=0; i<=n; i++)
+      if(i%2==0)
+        sum+=(ulong)i;
+    return sum;
   }
-  static void SortArray(int[] arr){
-    for(int i=0; i<arr.Length-1; i++)
-      for(int j=i+1; j<arr.Length; j++){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
+  static ulong TinhTong2(int n){//dùng continue
+    ulong sum = 0;
+    for(int i=0; i<=n; i++){
+      if(i%2!=0)
+        continue;
+      sum+=(ulong)i;
+    }
+    return sum;
+  }
+  static ulong TinhTong3(int n){//dùng tăng 2 đơn vị
+    ulong sum = 0;
+    for(int i=0; i<=n; i+=2)
+      sum+=(ulong)i;
+    return sum;
   }
   static void Main()
   {
     Console.Clear();
+    int n = 1000000;
     Timing timer = new Timing();
-    int[] arr = new int[100000];
-    timer.startTime();
-    RandomizeArray(arr);
-    timer.StopTime();
-    System.Console.WriteLine("RandomizeArray takes {0} seconds", timer.Result().TotalSeconds);
-    
-    timer.startTime();
-    SortArray(arr);
-    timer.StopTime();
-    System.Console.WriteLine("SortArray takes {0} seconds", timer.Result().TotalSeconds);
+    timer.startTime(); TinhTong1(n); timer.StopTime();
+    System.Console.WriteLine("Tong 1: " + timer.Result().TotalMilliseconds);
+
+    timer.startTime(); TinhTong2(n); timer.StopTime();
+    System.Console.WriteLine("Tong 2: " + timer.Result().TotalMilliseconds);
+
+    timer.startTime(); TinhTong3(n); timer.StopTime();
+    System.Console.WriteLine("Tong 3: " + timer.Result().TotalMilliseconds);
     Console.ReadLine();
   }
 }
