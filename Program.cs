34a15c55b2cb923a717f09275bb9b07b121144e2
thanhdnh <myhoangthanh@yahoo.com>
2022-10-17 09:52:26 +0700

    OOK

--- Program.cs
+++ Program.cs
@@ -32,11 +32,31 @@
       System.Console.WriteLine();
     }
   }
+  static void GenerateList<T>(List<T> list){
+    Random r = new Random();
+    for(int i=0; i<list.Count; i++)
+      list.Add((dynamic)r.Next());
+  }
+  static T SumList<T>(List<T> list){
+    T sum = list[0];
+    for(int i=1; i<list.Count; i++)
+      sum += (dynamic)list[i];
+    return sum;
+  }
   static void Main(){
     Console.Clear();
-    Array arr2d;  int[] len = {2, 3}; int[] lb = {0, 0};
+    /*List<int> intlist = new List<int>();
+    intlist.Add(3); intlist.Add(5); intlist.Add(7);
+    System.Console.WriteLine("Tong cua list: " + SumList<int>(intlist));
+
+    List<List<int>> mylist = new List<List<int>>();
+    List<int> sub = new List<int>{1, 3, 5};
+    mylist.Add(sub);*/
+
+    /*Array arr2d;  int[] len = {2, 3}; int[] lb = {0, 0};
     Create2DArray(out arr2d, len, lb);
-    Print2DArray(arr2d);
+    Print2DArray(arr2d);*/
+
     /*Array arr;
     CreateArray(out arr, 5, 0);
     System.Console.WriteLine("Tong: " + SumArray(arr));
