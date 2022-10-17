dd8d6d8d71508aa1835092d41d265e3992b8ca08
thanhdnh <myhoangthanh@yahoo.com>
2022-10-17 09:09:29 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -18,13 +18,29 @@
       sum += v;
     return sum;
   }
-  static void Main()
-  {
+  static void Create2DArray(out Array arr, int[] length, int[] lowerbound){
+    Random r = new Random();
+    arr = Array.CreateInstance(typeof(int), length, lowerbound);
+    for(int i=arr.GetLowerBound(0); i<=arr.GetUpperBound(0); i++)
+      for(int j=arr.GetLowerBound(1); i<=arr.GetUpperBound(1); i++)
+        arr.SetValue(r.NextSingle()*10, i, j);
+  }
+  static void Print2DArray(Array arr){
+    for(int i=arr.GetLowerBound(0); i<=arr.GetUpperBound(0); i++){
+      for(int j=arr.GetLowerBound(1); i<=arr.GetUpperBound(1); i++)
+        System.Console.Write("{0, 5}", arr.GetValue(i, j));
+      System.Console.WriteLine();
+    }
+  }
+  static void Main(){
     Console.Clear();
-
-    Array arr;
+    Array arr2d;  int[] len = {2, 3}; int[] lb = {0, 0};
+    Create2DArray(out arr2d, len, lb);
+    Print2DArray(arr2d);
+    /*Array arr;
     CreateArray(out arr, 5, 0);
     System.Console.WriteLine("Tong: " + SumArray(arr));
+    */
 
     Console.ReadLine();
   }
