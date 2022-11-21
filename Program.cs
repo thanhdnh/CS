62985ee1e36136db2bcf63695b5e4564674c9dce
thanhdnh <myhoangthanh@yahoo.com>
2022-11-21 09:19:06 +0700

    OKKK

--- Program.cs
+++ Program.cs
@@ -152,19 +152,20 @@
     binaryTree.Insert(23); binaryTree.Insert(16); binaryTree.Insert(45); 
     binaryTree.Insert(3); binaryTree.Insert(22); binaryTree.Insert(37); 
     binaryTree.Insert(99);
-    //Console.WriteLine(">> Max:" + binaryTree.FindMax());  //hoặc dùng binaryTree.FindMax2()   
-    //Console.WriteLine(">> Min:" + binaryTree.FindMin());  //hoặc dùng binaryTree.FindMin2()
-    //Node node = binaryTree.Find(5);
-    //int depth = binaryTree.GetTreeDepth();
+    Console.WriteLine(">> Max:" + binaryTree.FindMax());  //hoặc dùng binaryTree.FindMax2()   
+    Console.WriteLine(">> Min:" + binaryTree.FindMin());  //hoặc dùng binaryTree.FindMin2()
+    Node node = binaryTree.Find(5);
+    int depth = binaryTree.GetTreeDepth();
     Console.WriteLine("\n>> InOrder Traversal:"); 
     binaryTree.TraverseInOrder(binaryTree.Root);
     Console.WriteLine("\n>> PreOrder Traversal:"); 
     binaryTree.TraversePreOrder(binaryTree.Root);
     Console.WriteLine("\n>> PostOrder Traversal:"); 
     binaryTree.TraversePostOrder(binaryTree.Root);
-    //binaryTree.Remove(7); binaryTree.Remove(8);
-    //Console.WriteLine("\n>> PreOrder After Removing Operation:");
-    //binaryTree.TraversePreOrder(binaryTree.Root);
+    System.Console.WriteLine("\n\nAfter removing Nodes 7 and 8:");
+    binaryTree.Remove(7); binaryTree.Remove(8);
+    Console.WriteLine("\n>> PreOrder After Removing Operation:");
+    binaryTree.TraversePreOrder(binaryTree.Root);
 
     Console.ReadLine();
   }
